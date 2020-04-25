f6f77ad1666c885d8a89347c6207fbb93bbfa382
Davi Schmitz <davi.schmitz@hotmail.com>
2020-04-24 23:43:39 -0300

    Add bitbucket files

--- README.md
+++
@@ -1,1 +0,0 @@
-# udpsocket-chat
---
+++ udpsocket-chat/mainwindow.h
@@ -0,0 +1,35 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "qudpsocket.h"
+#include <QAbstractSocket>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_pushButtonMonitoringConnect_clicked();
+
+    void on_pushButtonConnect_clicked();
+
+    void on_pushButtonSend_clicked();
+
+    void dataArrived();
+
+private:
+    Ui::MainWindow *ui;
+
+    QUdpSocket * socket;
+};
+
+#endif // MAINWINDOW_H
